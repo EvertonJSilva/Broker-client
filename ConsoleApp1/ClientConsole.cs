8b28e2d31a9cec16ca3cf205e8c56c6d75b37d7b
EvertonJSilva <evertonjdasilva@yahoo.com.br>
2018-12-01 11:32:37 -0200

    inicial

---
+++ ConsoleApp1/ClientConsole.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FourHealth.Domain.Entities;
+
+namespace ConsoleApp1
+{
+    class ClientConsole
+    {
+        static void Main(string[] args)
+        {
+            bool sair = false;
+            while (!sair)
+            {
+                Console.WriteLine("\nPara consultar digite 1, para incluir digite 2:");
+
+                if (int.Parse(Console.ReadLine()) == 1)
+                {
+                    consultar();
+                }
+                else
+                {
+                    inserir();
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        public static void consultar()
+        {
+            Console.WriteLine("\nDigite o id para consulta:");
+
+            int id = int.Parse(Console.ReadLine());
+            Beneficiario beneficiario = null;
+
+            try
+            {
+                beneficiario = Proxy.ConsultarBeneficiarioPorId(id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            if (beneficiario == null)
+                Console.WriteLine("\nId não encontrado");
+            else
+                Console.WriteLine("\nCpf do beneficiario: " + beneficiario.Cpf);
+
+        }
+
+        public static void inserir()
+        {
+            Console.WriteLine("\nDigite o nome do beneficiario:");
+
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("\nDigite o CPF do beneficiario:");
+
+            string cpf = Console.ReadLine();
+
+            Beneficiario beneficiario = new Beneficiario();
+            beneficiario.Nome = nome;
+            beneficiario.Cpf = cpf;
+            beneficiario.Id = 0;
+
+            string retorno = "";
+            try
+            {
+               retorno = Proxy.IncluirBeneficiario(beneficiario);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine(retorno);
+          
+        }
+    }
+}
---
+++ ConsoleApp1/Proxy.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using FourHealth.Domain.Entities;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    public class Proxy
+    {
+
+        public static ServiceBroker.WebService1SoapClient  client = new ServiceBroker.WebService1SoapClient();
+
+        public static Beneficiario ConsultarBeneficiarioPorId(int id)
+        {
+            try
+            {
+                string ApiBaseUrl;
+                ApiBaseUrl = client.Broker_BuscarServidor();
+                
+                string MetodoPath = "/api/Beneficiario/" + id.ToString(); //caminho do método a ser chamado
+
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(ApiBaseUrl + MetodoPath);
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "GET";
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var valor = streamReader.ReadToEnd();
+                    if (valor.Substring(1, 1) == "[")
+                    {
+                        var retorno = JsonConvert.DeserializeObject<List<Beneficiario>>(valor);
+                        return retorno.FirstOrDefault();
+                    }
+                    else
+                    {
+                        var retorno = JsonConvert.DeserializeObject<Beneficiario>(valor);
+                        return retorno;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new System.Exception(ex.Message);
+            }
+        }
+
+        public static string IncluirBeneficiario(Beneficiario beneficiario)
+        {
+            try
+            {
+                string ApiBaseUrl;
+                ApiBaseUrl = client.Broker_BuscarServidor();
+                string MetodoPath = "/api/Beneficiario/"; //caminho do método a ser chamado
+
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(ApiBaseUrl + MetodoPath);
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    streamWriter.Write(JsonConvert.SerializeObject(beneficiario));
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var valor = streamReader.ReadToEnd();
+
+                    return valor.ToString();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new System.Exception(ex.Message);
+            }
+        }
+    }
+}
